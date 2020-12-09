d0a693b85346ad2a98358ec638f0ea7085e62bb8
Merlin Danner <merlin.danner@fau.de>
2020-12-09 13:14:19 +0100

    Update configure script

--- configure.sh
+++ configure.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# TODO install pistol
-
 CFG_DIR=$(readlink -f $(dirname "$0"))
 
 rootAccess=false
@@ -20,8 +18,9 @@ fi
 ln -sf "$CFG_DIR/vimrc" "$HOME/.vimrc"
 
 # tmux TODO tpm
-mkdir -p "$HOME/.config/tmux"
-ln -sf "$CFG_DIR/tmux.conf" "$HOME/.config/tmux/tmux.conf"
+mkdir -p "${XDG_CONFIG_HOME}/tmux/plugins"
+ln -sf "$CFG_DIR/tmux.conf" "${XDG_CONFIG_HOME}/tmux/tmux.conf"
+git clone "https://github.com/tmux-plugins/tpm" "${XDG_CONFIG_HOME}/tmux/plugins/tpm"
 
 # zsh
 mkdir -p "$ZDOTDIR"
@@ -29,27 +28,22 @@ ln -sf "$CFG_DIR/zshrc" "$ZDOTDIR/.zshrc"
 ln -sf "$CFG_DIR/zsh_aliases" "$ZDOTDIR/zsh_aliases"
 
 # polybar
-rm -rf "$HOME/.config/polybar"
-ln -sf "$CFG_DIR/polybar" "$HOME/.config/polybar"
+rm -rf "${XDG_CONFIG_HOME}/polybar"
+ln -sf "$CFG_DIR/polybar" "${XDG_CONFIG_HOME}/polybar"
 
 # bspwm
-rm -rf "$HOME/.config/bspwm"
-ln -sf "$CFG_DIR/bspwm" "$HOME/.config/bspwm"
+rm -rf "${XDG_CONFIG_HOME}/bspwm"
+ln -sf "$CFG_DIR/bspwm" "${XDG_CONFIG_HOME}/bspwm"
 
 # sxhkd
-rm -rf "$HOME/.config/sxhkd"
-ln -sf "$CFG_DIR/sxhkd" "$HOME/.config/sxhkd"
+rm -rf "${XDG_CONFIG_HOME}/sxhkd"
+ln -sf "$CFG_DIR/sxhkd" "${XDG_CONFIG_HOME}/sxhkd"
 
 # rofi
 rm -rf "${XDG_CONFIG_HOME}/rofi"
 mkdir -p "${XDG_CONFIG_HOME}/rofi"
 ln -sf "$CFG_DIR/config.rasi" "${XDG_CONFIG_HOME}/rofi"
 ln -sf "$CFG_DIR/hackerman.rasi" "${XDG_CONFIG_HOME}/rofi"
-
-# vifm TODO install pistol
-mkdir -p "$XDG_CONFIG_HOME/vifm/colors"
-ln -sf "$CFG_DIR/merlin.vifm" "$XDG_CONFIG_HOME/vifm/colors"
-ln -sf "$CFG_DIR/vifmrc" "$XDG_CONFIG_HOME/vifm"
 
 mkdir -p "$HOME/bin"
 ln -sf \
@@ -60,9 +54,6 @@ ln -sf \
 	"$CFG_DIR/scripts/setStatusbar" \
 	"$CFG_DIR/scripts/moveDesktopsToMonitors.sh" \
 	"$HOME/bin/"
-
-# Install zsh plugins
-. "$CFG_DIR/install.zsh.plugins.sh"
 
 # Build st
 cd "$CFG_DIR"
@@ -79,3 +70,21 @@ fi
 
 cd -
 rm -rf "st-0.8.4/" "st-0.8.4.tar.gz"
+
+# Enable systemd services
+
+sudo systemctl enable --now NetworkManager
+sudo systemctl enable ly.service
+
+# install vundle if necessary
+if ! [ -d "${HOME}/.vim/bundle/Vundle.vim" ]; then
+	mkdir -p "${HOME}/.vim/bundle/"
+	git clone 'https://github.com/VundleVim/Vundle.vim.git' "${HOME}/.vim/bundle/Vundle.vim"
+	vim +PluginInstall +PluginClean +qall
+	cd "${HOME}/.vim/bundle/vim-hexokinase/"
+	make hexokinase
+	cd -
+	cd "${HOME}/.vim/bundle/youcompleteme"
+	./install.py --all
+	cd -
+fi
