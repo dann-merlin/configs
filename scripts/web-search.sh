d078c8cd90063b4203b65f2c8af44dd287214d22
Merlin Danner <merlin.danner@fau.de>
2021-01-27 21:58:32 +0100

    Add rofi scripts

---
+++ scripts/web-search.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Info:
+#   author:    Miroslav Vidovic
+#   file:      web-search.sh
+#   created:   24.02.2017.-08:59:54
+#   revision:  ---
+#   version:   1.0
+# -----------------------------------------------------------------------------
+# Requirements:
+#   rofi
+# Description:
+#   Use rofi to search the web.
+# Usage:
+#   web-search.sh
+# -----------------------------------------------------------------------------
+# Script:
+
+declare -A URLS
+
+URLS=(
+  ["duckduckgo"]="https://www.duckduckgo.com/?q="
+  ["github"]="https://github.com/search?q="
+  ["stackoverflow"]="http://stackoverflow.com/search?q="
+  ["superuser"]="http://superuser.com/search?q="
+)
+
+# List for rofi
+gen_list() {
+    for i in "${!URLS[@]}"
+    do
+      echo "$i"
+    done
+}
+
+main() {
+  # Pass the list to rofi
+  platform=$( (gen_list) | rofi -dmenu -matching fuzzy -no-custom -location 0 -p "Search > " )
+
+  if [[ -n "$platform" ]]; then
+    query=$( (echo ) | rofi  -dmenu -matching fuzzy -location 0 -p "Query > " )
+
+    if [[ -n "$query" ]]; then
+      url=${URLS[$platform]}$query
+      xdg-open "$url"
+    else
+      exit
+    fi
+
+  else
+    exit
+  fi
+}
+
+main
+
+exit 0
